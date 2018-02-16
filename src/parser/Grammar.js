950f94c2627f79c23fe2911298240c9f5c1e0b0d
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-16 14:16:59 +0000

    add grammar for parser

---
+++ src/parser/Grammar.js
@@ -0,0 +1,30 @@
+module.exports = {
+  whitespace: '\n\t '.split(),
+  comments: [
+    ['/*', '*/'],
+    ['%', '\n'],
+    ['#', '\n']
+  ],
+  singleSymbols: [
+    '(', // start of arguments or tuple
+    ')', // start of arguments or tuple
+    '.', // end of clause
+    ';', // OR operator 
+    ',', // argument separator or AND operator
+    '-', // subtraction / binary
+    '+', // addition / binary
+    '*', // multiplication / binary
+    '=', // equality / binary
+    '/', // division / binary
+    '^', // power to / binary
+    '>', // more than / binary
+    '<', // less than / binary
+    '!' // negation / unary
+  ],
+  doubleSymbol: [
+    '->', // implies / binary
+    '<-', // implied by / binary
+    '<=', // less than or equal / binary
+    '>=' // more than or equal / binary
+  ]
+};
