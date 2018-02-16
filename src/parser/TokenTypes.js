4a76a0212a6fc7f13a3e8dad0b7db787aff8e110
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-16 15:38:28 +0000

    add token types

---
+++ src/parser/TokenTypes.js
@@ -0,0 +1,10 @@
+module.exports = {
+  Variable: Symbol(),
+  Symbol: Symbol(),
+  Constant: Symbol(),
+  Number: Symbol(),
+  Eof: Symbol(),
+  Whitespace: Symbol(),
+  Comment: Symbol(),
+  Error: Symbol()
+};
