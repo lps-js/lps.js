2f179c256464eaea3e08630e49bc409b29a79a1c
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-16 18:16:51 +0000

    update node types

--- src/parser/NodeTypes.js
+++ src/parser/NodeTypes.js
@@ -1,9 +1,12 @@
 module.exports = {
   Program: Symbol('Program'),
   Clause: Symbol('Clause'),
+  LiteralSet: Symbol('LiteralSet'),
   Literal: Symbol('Literal'),
+  LiteralArguments: Symbol('LiteralArguments'),
   Constant: Symbol('Constant'),
   Variable: Symbol('Variable'),
   Function: Symbol('Function'),
+  FunctionArguments: Symbol('FunctionArguments'),
   Symbol: Symbol('Symbol')
 };
