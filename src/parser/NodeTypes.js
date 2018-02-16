7c51a972034be148a7ab277616c6fe8859a9f365
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-16 16:34:17 +0000

    add ast nodetypes

---
+++ src/parser/NodeTypes.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Program: Symbol('Program'),
+  Clause: Symbol('Clause'),
+  Literal: Symbol('Literal'),
+  Constant: Symbol('Constant'),
+  Variable: Symbol('Variable'),
+  Function: Symbol('Function'),
+  Symbol: Symbol('Symbol')
+};
