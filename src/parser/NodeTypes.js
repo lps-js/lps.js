ed9aaf7811c71c504540d0bde5ba8075f808e91d
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-20 10:44:14 +0000

    update node types

--- src/parser/NodeTypes.js
+++ src/parser/NodeTypes.js
@@ -2,14 +2,11 @@ module.exports = {
   Program: Symbol('Program'),
   Clause: Symbol('Clause'),
   LiteralSet: Symbol('LiteralSet'),
-  Literal: Symbol('Literal'),
-  LiteralName: Symbol('LiteralName'),
   Constant: Symbol('Constant'),
   Variable: Symbol('Variable'),
-  Function: Symbol('Function'),
   Number: Symbol('Number'),
-  FunctionName: Symbol('FunctionName'),
-  Arguments: Symbol('Arguments'),
-  Expression: Symbol('Expression'),
+  Functor: Symbol('Functor'),
+  BinaryOperator: Symbol('BinaryOperator'),
+  UnaryOperator: Symbol('UnaryOperator'),
   Symbol: Symbol('Symbol')
 };
