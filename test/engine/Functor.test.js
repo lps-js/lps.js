9ca2ff5db401e5a850bedd142c850671198023da
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-25 01:06:08 +0000

    add partial test for functor

---
+++ test/engine/Functor.test.js
@@ -0,0 +1,19 @@
+const Functor = require('../../src/engine/Functor');
+const Value = require('../../src/engine/Value');
+const Variable = require('../../src/engine/Variable');
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Functor', () => {
+
+  describe('getVariables()', () => {
+    it('should return variables in itself', () => {
+      let functor = new Functor('add', [new Variable('X'), new Value(5)]);
+      expect(functor.getVariables).to.be.a('function');
+
+      expect(functor.getVariables()).to.be.an('array');
+    });
+  });
+
+});
