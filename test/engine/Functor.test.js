7e807ed0eacde6ffff9c8b36ad1330c65a637c4b
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-25 01:18:09 +0000

    add constructor test in Functor

--- test/engine/Functor.test.js
+++ test/engine/Functor.test.js
@@ -7,6 +7,23 @@ const expect = chai.expect;
 
 describe('Functor', () => {
 
+  describe('constructor', () => {
+    it('should initialise the Functor correctly', () => {
+      let arguments = [new Variable('X'), new Value(5)];
+      let functor = new Functor('add', arguments);
+      expect(functor.getId).to.be.a('function');
+
+      expect(functor.getId()).to.be.equals("add/2");
+
+      expect(functor.getArguments).to.be.a('function');
+
+      expect(functor.getArguments()).to.be.an('array');
+      expect(functor.getArguments()).contains(arguments[0]);
+      expect(functor.getArguments()).contains(arguments[1]);
+      expect(functor.getArguments().length).to.be.equals(2);
+    });
+  });
+
   describe('getVariables()', () => {
     it('should return variables in itself', () => {
       let functor = new Functor('add', [new Variable('X'), new Value(5)]);
