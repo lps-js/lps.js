19aa5e31c6c9ec06b3adb49fa6d7dabeb0718676
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-01 02:09:54 +0000

    add art node initialise test

--- test/parser/AstNode.test.js
+++ test/parser/AstNode.test.js
@@ -4,5 +4,13 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('AstNode', () => {
-
+  describe('initialize AstNode', () => {
+    it('should initialise correct with type and token', () => {
+      let symbol = Symbol();
+      let token = {};
+      let node = new AstNode(symbol, token);
+      expect(node.getType()).to.be.equal(symbol);
+      expect(node.getToken()).to.be.equal(token);
+    });
+  });
 });
