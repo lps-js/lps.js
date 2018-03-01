35937ef33e7cf9c5c3745f031ff2a124a08ff670
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-01 02:28:50 +0000

    add tests for AstNode

--- test/parser/AstNode.test.js
+++ test/parser/AstNode.test.js
@@ -2,6 +2,7 @@ const AstNode = require('../../src/parser/AstNode');
 
 const chai = require('chai');
 const expect = chai.expect;
+require('mocha-sinon');
 
 describe('AstNode', () => {
   describe('initialize AstNode', () => {
@@ -11,6 +12,33 @@ describe('AstNode', () => {
       let node = new AstNode(symbol, token);
       expect(node.getType()).to.be.equal(symbol);
       expect(node.getToken()).to.be.equal(token);
+      expect(node.getChildren()).to.be.an('array');
+      expect(node.getChildren()).to.be.empty;
+      expect(node.isLeaf()).to.be.equal(true);
+    });
+  });
+
+  describe('setToken', () => {
+    it('should change the token', () => {
+      let symbol = Symbol();
+      let token1 = {};
+      let token2 = {'test': true};
+      let node = new AstNode(symbol, token1);
+      expect(node.getToken()).to.be.equal(token1);
+      node.setToken(token2);
+      expect(node.getToken()).to.be.equal(token2);
+    });
+  });
+
+  describe('print', () => {
+    it('should change the token', function() {
+      this.sinon.stub(console, 'log');
+      let symbol = Symbol();
+      let token = {'value': '.'};
+      let node = new AstNode(symbol, token);
+      node.print();
+      expect( console.log.calledOnce ).to.be.true;
+      this.sinon.restore();
     });
   });
 });
