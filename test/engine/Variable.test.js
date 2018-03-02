bb27446cf409c4cfbada94f104dba2a79adf0325
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-02 01:42:15 +0000

    lint complain fixes

--- test/engine/Variable.test.js
+++ test/engine/Variable.test.js
@@ -5,13 +5,12 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('Variable', () => {
-
   describe('evaluate', () => {
     it('should return the variable name', () => {
       let value = new Variable('X');
       expect(value.evaluate).to.be.a('function');
 
-      expect(value.evaluate()).to.be.equals("X");
+      expect(value.evaluate()).to.be.equals('X');
     });
   });
 
@@ -20,19 +19,19 @@ describe('Variable', () => {
       let value = new Variable('X');
       expect(value.substitute).to.be.a('function');
 
-      let theta = {X : new Value(5)};
+      let theta = { X: new Value(5) };
       expect(value.substitute(theta)).to.be.instanceof(Value);
-      expect(value.substitute(theta)).to.be.equals(theta['X']);
+      expect(value.substitute(theta)).to.be.equals(theta.X);
       expect(value.substitute(theta).evaluate()).to.be.equals(5);
     });
-      it('should return the substituted value if theta contains it', () => {
-        let value = new Variable('X');
-        expect(value.substitute).to.be.a('function');
 
-        let theta = {Y : new Value(5)};
-        expect(value.substitute(theta)).to.be.instanceof(Variable);
-        expect(value.substitute(theta).evaluate()).to.be.equals('X');
-      });
+    it('should return the substituted value if theta contains it', () => {
+      let value = new Variable('X');
+      expect(value.substitute).to.be.a('function');
+
+      let theta = { Y: new Value(5) };
+      expect(value.substitute(theta)).to.be.instanceof(Variable);
+      expect(value.substitute(theta).evaluate()).to.be.equals('X');
+    });
   });
-
 });
