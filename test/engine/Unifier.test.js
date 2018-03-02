9748c835a21af7a8142073bb730b2a402eb7a619
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-02 01:24:59 +0000

    add more test for unifier

--- test/engine/Unifier.test.js
+++ test/engine/Unifier.test.js
@@ -93,6 +93,16 @@ describe('Unifier', () => {
     expect(theta['X'].evaluate()).to.be.equal(2);
   });
 
+  it('should unify two functors with nesting, and return correct substution', () => {
+    let value1 = new Functor('test', [new Functor('g', [new Variable('Y')])]);
+    let value2 = new Functor('test', [new Variable('X')]);
+    let theta = Unifier([[value1, value2]]);
+    expect(theta).to.be.not.null;
+    expect(Object.keys(theta)).to.be.contain('X');
+    expect(theta['X']).to.be.instanceof(Functor)
+    expect(theta['X'].getId()).to.be.equal('g/1');
+  });
+
   it('should not unify two funtors with an incorrect affecting substitution', () => {
     let value1 = new Functor('test', [new Value(1), new Value(2)]);
     let value2 = new Functor('test', [new Value(1), new Variable('X')]);
