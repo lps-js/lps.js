2956b68e91fba4ffe4bf0e9b6e9794e1ecc3324e
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-03 01:57:50 +0000

    add test for variableArrayRename

---
+++ test/utility/variableArrayRename.test.js
@@ -0,0 +1,20 @@
+const Variable = require('../../src/engine/Variable');
+const variableArrayRename = require('../../src/utility/variableArrayRename');
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('variableArrayRename', () => {
+  it('should return an empty array if given an empty one', () => {
+    let theta = variableArrayRename([]);
+    expect(Object.keys(theta)).to.be.empty;
+  });
+
+  it('should return a correct substitution given a variable', () => {
+    let theta = variableArrayRename([new Variable('X')]);
+    expect(Object.keys(theta)).to.be.not.empty;
+    expect(theta.X).to.be.not.equal(null);
+    expect(theta.X).to.be.instanceof(Variable);
+    expect(theta.X.evaluate()).to.be.equal('VXX');
+  });
+});
