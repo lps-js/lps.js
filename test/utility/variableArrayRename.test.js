bc22cd6ddb6fa289bb491699017588a5e79dfd4f
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-03 02:01:10 +0000

    add additional variableArrayRename test

--- test/utility/variableArrayRename.test.js
+++ test/utility/variableArrayRename.test.js
@@ -21,4 +21,12 @@ describe('variableArrayRename', () => {
   it('should throw error when invalid rename pattern given', () => {
     expect(() => { variableArrayRename([new Variable('X')], 5) }).to.throw();
   });
+
+  it('should return a correct substitution given a variable and rename pattern', () => {
+    let theta = variableArrayRename([new Variable('X')], '_aa*');
+    expect(Object.keys(theta)).to.be.not.empty;
+    expect(theta.X).to.be.not.equal(null);
+    expect(theta.X).to.be.instanceof(Variable);
+    expect(theta.X.evaluate()).to.be.equal('_aaX');
+  });
 });
