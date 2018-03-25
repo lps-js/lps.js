aa03f2c26fc4853464eef590329e84904faa7fbe
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-25 22:33:02 +0100

    update test for resolution of clause with two variables

--- test/engine/Clause.test.js
+++ test/engine/Clause.test.js
@@ -17,6 +17,7 @@ describe('Clause', () => {
       expect(resolution).to.be.instanceof(Clause);
       expect(resolution.getVariables()).to.be.empty;
       expect(resolution.getHeadLiterals().length).to.be.equal(1);
+
       let headFunctor = resolution.getHeadLiterals()[0];
       expect(headFunctor).to.be.instanceof(Functor);
       expect(headFunctor.getId()).to.be.equal('test1/1');
@@ -29,7 +30,20 @@ describe('Clause', () => {
       let head = [new Functor('test1', [new Variable('X'), new Variable('Y')])];
       let body = [new Functor('test2', [new Variable('X')])];
       let clause = new Clause(head, body);
-      console.log(clause.resolve(new Functor('test2', [new Value(5)])).toString());
+
+      let resolution = clause.resolve(new Functor('test2', [new Value(5)]));
+      expect(resolution).to.be.instanceof(Clause);
+      expect(resolution.getVariables().length).to.be.equal(1);
+      expect(resolution.getHeadLiterals().length).to.be.equal(1);
+
+      let headFunctor = resolution.getHeadLiterals()[0];
+      expect(headFunctor).to.be.instanceof(Functor);
+      expect(headFunctor.getId()).to.be.equal('test1/2');
+      expect(headFunctor.getVariables().length).to.be.equal(1);
+      expect(headFunctor.getVariables()[0]).to.be.equal('Y');
+      expect(headFunctor.getArguments().length).to.be.equal(2);
+      expect(headFunctor.getArguments()[0].evaluate()).to.be.equal(5);
+      expect(headFunctor.getArguments()[1]).to.be.instanceof(Variable);
     });
   });
 });
