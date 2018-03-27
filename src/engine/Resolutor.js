6ee11a52965d1eca52076719e75e7d44012eca3e
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-27 23:12:57 +0100

    add action resolution in resolutor

--- src/engine/Resolutor.js
+++ src/engine/Resolutor.js
@@ -52,7 +52,46 @@ Resolutor.resolve = function resolve(clause, fact) {
 };
 
 Resolutor.resolveAction = function resolveAction(clause, action) {
+  let substitutedAction = action
+    .substitute(variableArrayRename(action.getVariables(), '$fv_*'));
+  let theta = {};
+  let unresolvedHeadLiterals = [];
+  let _head = clause.getHeadLiterals();
+  let _body = clause.getBodyLiterals();
 
+  _head.forEach((literal) => {
+    let newTheta = Unifier.unifies([[substitutedAction, literal]], theta);
+    if (newTheta === null) {
+      // unable to unify, let's just add to unresolvedBodyLiterals
+      unresolvedHeadLiterals.push(literal);
+    } else {
+      theta = newTheta;
+    }
+  });
+
+  if (unresolvedHeadLiterals.length === _head.length) {
+    // nothing got resolved, probably not a matching rule.
+    return null;
+  }
+
+  // perform substitution here
+  unresolvedHeadLiterals = unresolvedHeadLiterals.map((literal) => {
+    return literal.substitute(theta);
+  });
+
+  // perform head check
+  for (let i = 0; i < unresolvedHeadLiterals.length; i += 1) {
+    let literal = unresolvedBodyLiterals[i];
+    if ((literal instanceof BooleanBinaryOperator
+          || literal instanceof BooleanUnaryOperator)
+        && literal.isGround() && !literal.evaluate()) {
+      // nope this doesn't work out
+      return null;
+    }
+  }
+
+  let newBody = _body.map(expressions => expressions.substitute(theta));
+  return new Clause(unresolvedHeadLiterals, newBody);
 };
 
 module.exports = Resolutor;
