f94573264d30f624a16e4b9c283270cecf403ab4
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-02 13:11:41 +0100

    update test for Clause

--- test/engine/Clause.test.js
+++ test/engine/Clause.test.js
@@ -27,6 +27,8 @@ describe('Clause', () => {
       expect(clause.getBodyLiterals()).to.be.instanceof(Array);
       expect(clause.getHeadLiterals()).to.be.length(1);
       expect(clause.getBodyLiterals()).to.be.length(0);
+      expect(clause.getHeadLiteralsCount()).to.be.equal(1);
+      expect(clause.getBodyLiteralsCount()).to.be.equal(0);
 
       expect(clause.getHeadLiterals()[0]).to.be.instanceof(Functor);
       expect(clause.getHeadLiterals()[0].getId()).to.be.equal('test/1');
@@ -64,6 +66,211 @@ describe('Clause', () => {
         [new Functor('test', [new Variable('X')])]
       );
       expect(clause.isGround()).to.be.false;
+    });
+  });
+
+  describe('substitute', () => {
+    it('should return correct substitution for empty substitution in a ground fact', () => {
+      let clause = new Clause(
+        [new Functor('test', [new Value(5)])],
+        []
+      );
+      let theta = {};
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for some substitution in a ground fact', () => {
+      let clause = new Clause(
+        [new Functor('test', [new Value(5)])],
+        []
+      );
+      let theta = { 'X' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for empty substitution in a unground fact', () => {
+      let clause = new Clause(
+        [new Functor('test', [new Variable('X')])],
+        []
+      );
+      let theta = {};
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for some substitution in a unground fact', () => {
+      let clause = new Clause(
+        [new Functor('test', [new Variable('X')])],
+        []
+      );
+      let theta = { 'X' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal('test(3).');
+    });
+
+    it('should return same clause for some irrelevant substitution in a unground fact', () => {
+      let clause = new Clause(
+        [new Functor('test', [new Variable('X')])],
+        []
+      );
+      let theta = { 'T' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for empty substitution in a ground query', () => {
+      let clause = new Clause(
+        [],
+        [new Functor('test', [new Value(5)])]
+      );
+      let theta = {};
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for some substitution in a ground query', () => {
+      let clause = new Clause(
+        [],
+        [new Functor('test', [new Value(5)])]
+      );
+      let theta = { 'X' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for empty substitution in a unground query', () => {
+      let clause = new Clause(
+        [],
+        [new Functor('test', [new Variable('X')])]
+      );
+      let theta = {};
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for some substitution in a unground query', () => {
+      let clause = new Clause(
+        [],
+        [new Functor('test', [new Variable('X')])]
+      );
+      let theta = { 'X' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal('<- test(3).');
+    });
+
+    it('should return same clause for some irrelevant substitution in a unground query', () => {
+      let clause = new Clause(
+        [],
+        [new Functor('test', [new Variable('X')])]
+      );
+      let theta = { 'T' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+
+    it('should return correct substitution for empty substitution in a ground clause', () => {
+      let clause = new Clause(
+        [new Functor('test2', [new Value(5)])],
+        [new Functor('test', [new Value(5)])]
+      );
+      let theta = {};
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for some substitution in a ground clause', () => {
+      let clause = new Clause(
+        [new Functor('test2', [new Value(5)])],
+        [new Functor('test', [new Value(5)])]
+      );
+      let theta = { 'X' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for empty substitution in a unground clause', () => {
+      let clause = new Clause(
+        [new Functor('test2', [new Value(5)])],
+        [new Functor('test', [new Value(5)])]
+      );
+      let theta = {};
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+
+    it('should return correct substitution for some substitution in a unground clause', () => {
+      let clause = new Clause(
+          [new Functor('test2', [new Variable('X')])],
+          [new Functor('test', [new Variable('X')])]
+      );
+      let theta = { 'X' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal('test2(3) <- test(3).');
+    });
+
+    it('should return same clause for some irrelevant substitution in a unground clause', () => {
+      let clause = new Clause(
+          [new Functor('test2', [new Variable('X')])],
+          [new Functor('test', [new Variable('X')])]
+      );
+      let theta = { 'T' : new Value(3) };
+      let substitutedClause = clause.substitute(theta);
+      expect(substitutedClause.toString()).to.be.equal(clause.toString());
+    });
+  });
+
+  describe('getHeadLiteralsCount', () => {
+    it('should return correct head literal count for fact', () => {
+      let clause = new Clause(
+        [new Functor('test', [new Value(5)])],
+        []
+      );
+      expect(clause.getHeadLiteralsCount()).to.be.equal(1);
+    });
+
+    it('should return correct head literal count for query', () => {
+      let clause = new Clause(
+        [],
+        [new Functor('test', [new Value(5)])]
+      );
+      expect(clause.getHeadLiteralsCount()).to.be.equal(0);
+    });
+
+    it('should return correct head literal count for clause', () => {
+      let clause = new Clause(
+        [new Functor('test2', [new Value(5)])],
+        [new Functor('test', [new Value(5)])]
+      );
+      expect(clause.getHeadLiteralsCount()).to.be.equal(1);
+    });
+  });
+
+  describe('getBodyLiteralsCount', () => {
+    it('should return correct head literal count for fact', () => {
+      let clause = new Clause(
+        [new Functor('test', [new Value(5)])],
+        []
+      );
+      expect(clause.getBodyLiteralsCount()).to.be.equal(0);
+    });
+
+    it('should return correct head literal count for query', () => {
+      let clause = new Clause(
+        [],
+        [new Functor('test', [new Value(5)])]
+      );
+      expect(clause.getBodyLiteralsCount()).to.be.equal(1);
+    });
+
+    it('should return correct head literal count for clause', () => {
+      let clause = new Clause(
+        [new Functor('test2', [new Value(5)])],
+        [new Functor('test', [new Value(5)])]
+      );
+      expect(clause.getBodyLiteralsCount()).to.be.equal(1);
     });
   });
 
@@ -114,6 +321,30 @@ describe('Clause', () => {
         [new Functor('test', [new Variable('X')])]
       );
       expect(clause.toString()).to.be.equal('test2(X) <- test(X).');
+    });
+
+    it('should return correct representation for multiple head literals clause', () => {
+      let clause = new Clause(
+        [
+          new Functor('test2', [new Variable('X')]),
+          new Functor('test3', [new Variable('Y')])
+        ],
+        [new Functor('test', [new Variable('X')])]
+      );
+      expect(clause.toString()).to.be.equal('test2(X), test3(Y) <- test(X).');
+    });
+
+    it('should return correct representation for multiple body literals clause', () => {
+      let clause = new Clause(
+        [
+          new Functor('test2', [new Variable('X')])
+        ],
+        [
+          new Functor('test', [new Variable('X')]),
+          new Functor('test3', [new Variable('Y')])
+        ]
+      );
+      expect(clause.toString()).to.be.equal('test2(X) <- test(X), test3(Y).');
     });
   });
 });
