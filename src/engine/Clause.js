6ae778ea14eefd9fe8d8e5104ec20cb77dda472e
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-02 12:54:55 +0100

    remove resolve code from Clause

--- src/engine/Clause.js
+++ src/engine/Clause.js
@@ -75,46 +75,6 @@ function Clause(head, body) {
     return [].concat(_body);
   };
 
-  this.resolve = function resolve(fact) {
-    let substitutedFact = fact
-      .substitute(variableArrayRename(fact.getVariables(), '$fv_*'));
-    let theta = {};
-    let unresolvedBodyLiterals = [];
-    _body.forEach((literal) => {
-      let newTheta = Unifier.unifies([[substitutedFact, literal]], theta);
-      if (newTheta === null) {
-        // unable to unify, let's just add to unresolvedBodyLiterals
-        unresolvedBodyLiterals.push(literal);
-      } else {
-        theta = newTheta;
-      }
-    });
-
-    if (unresolvedBodyLiterals.length === _body.length) {
-      // nothing got resolved, probably not a matching rule.
-      return null;
-    }
-
-    // perform substitution here
-    unresolvedBodyLiterals = unresolvedBodyLiterals.map((literal) => {
-      return literal.substitute(theta);
-    });
-
-    // perform head check
-    for (let i = 0; i < unresolvedBodyLiterals.length; i += 1) {
-      let literal = unresolvedBodyLiterals[i];
-      if ((literal instanceof BooleanBinaryOperator
-            || literal instanceof BooleanUnaryOperator)
-          && literal.isGround() && !literal.evaluate()) {
-        // nope this doesn't work out
-        return null;
-      }
-    }
-
-    let newHead = _head.map(expressions => expressions.substitute(theta));
-    return new Clause(newHead, unresolvedBodyLiterals);
-  };
-
   this.toString = function toString() {
     let result = '';
     for (let i = 0; i < _head.length; i += 1) {
