745ec3187b21b527c4ecaed3a17375109d7609ae
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-03 16:04:10 +0100

    remove commented code from index

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const Engine = require('./src/engine/Engine');
 
 Parser.parseFile('examples/fire2.lps')
   .then((token) => {
-    //token.print();
-
     let engine = new Engine(token);
     let result = engine.run();
     result.forEach((e) => {
