167b94a1927a14c3d0be2d5b2879ef56e38532ad
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-03 15:59:32 +0100

    update to use Engine.run()

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Parser = require('./src/parser/Parser');
 const Engine = require('./src/engine/Engine');
 
-Parser.parseFile('examples/fireSimple.lps')
+Parser.parseFile('examples/fire2.lps')
   .then((token) => {
-    token.print();
+    //token.print();
 
     let engine = new Engine(token);
+    let result = engine.run();
+    result.forEach((e) => {
+      console.log(e);
+    })
   });
