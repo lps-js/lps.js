5ccb664944d6fe0cd85432522dd4f88f6b5d5453
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-04 00:18:56 +0100

    update action check for resolutor query

--- src/engine/Resolutor.js
+++ src/engine/Resolutor.js
@@ -73,7 +73,7 @@ Resolutor.query = function query(program, clause, query, actions) {
     return result;
   }
 
-  if (actions !== undefined && query instanceof Functor && actions.indexOf(query.getId()) > -1) {
+  if (actions !== undefined && query instanceof Functor && actions[query.getId()] !== undefined) {
     return [{
       theta: {},
       actions: [
@@ -108,7 +108,7 @@ Resolutor.query = function query(program, clause, query, actions) {
   }
   let headLiteral = resolution.clause.getHeadLiterals()[0];
 
-  if (actions !== undefined && actions.indexOf(headLiteral.getId()) > -1) {
+  if (actions !== undefined && actions[headLiteral.getId()] !== undefined) {
     return [{
       theta: resolution.theta,
       actions: [
