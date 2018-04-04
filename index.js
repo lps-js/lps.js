b6a87d73f44a77d8359750ce86ec5f27aedf9faa
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-04 01:18:22 +0100

    update index file

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Parser = require('./src/parser/Parser');
 const Engine = require('./src/engine/Engine');
 
 Parser.parseFile('examples/fire2.lps')
-  .then((token) => {
-    let engine = new Engine(token);
+  .then((root) => {
+    let engine = new Engine(root);
     let result = engine.run();
     result.forEach((e) => {
       console.log(e);
-    })
+    });
   });
