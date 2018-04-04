a2e72b3f7a491042d59beec14bc110a34b4cd24b
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-04 13:22:17 +0100

    update code to print JSON result instead

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const Parser = require('./src/parser/Parser');
 const Engine = require('./src/engine/Engine');
 
-Parser.parseFile('examples/fire2.lps')
+Parser.parseFile('examples/fireSimple.lps')
   .then((root) => {
     let engine = new Engine(root);
     let result = engine.run();
-    result.forEach((e) => {
-      console.log(e);
-    });
+    console.log(JSON.stringify(result));
   });
