c797bcd993bb3a8b77ab876cb13967eae799c440
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-08 21:13:29 +0100

    add additional tests for resolutor

--- test/engine/Resolutor.test.js
+++ test/engine/Resolutor.test.js
@@ -295,6 +295,88 @@ describe('Resolutor', () => {
       expect(array[1].getId()).to.be.equal('test/1');
       expect(array[1].isGround()).to.be.true;
     });
+
+    it('should return null for constraints', () => {
+      let clause = new Clause(
+        [],
+        [
+          new Functor('test2', [new Variable('X')])
+        ]
+      );
+      let facts = new LiteralTreeMap();
+      facts.add(new Functor('test2', [new Value(5)]));
+      facts.add(new Functor('test2', [new Value(3)]));
+
+      let program = [clause];
+
+      let resolutor = new Resolutor(program, facts);
+      let newFacts = resolutor.resolve();
+
+      expect(newFacts).to.be.equal(null);
+    });
+
+    it('should return null for constraints', () => {
+      let clause1 = new Clause(
+        [
+          new Functor('test2', [new Variable('X')])
+        ],
+        [
+          new Functor('test1', [new Variable('X')])
+        ]
+      );
+      let clause2 = new Clause(
+        [],
+        [
+          new Functor('test2', [new Variable('X')])
+        ]
+      );
+      let facts = new LiteralTreeMap();
+      facts.add(new Functor('test1', [new Value(5)]));
+      facts.add(new Functor('test1', [new Value(3)]));
+
+      let program = [clause1, clause2];
+
+      let resolutor = new Resolutor(program, facts);
+      let newFacts = resolutor.resolve();
+
+      expect(newFacts).to.be.equal(null);
+    });
+
+    it('should return correct resolution for constraints', () => {
+      let clause1 = new Clause(
+        [
+          new Functor('test2', [new Variable('X')])
+        ],
+        [
+          new Functor('test1', [new Variable('X')])
+        ]
+      );
+      let clause2 = new Clause(
+        [],
+        [
+          new Functor('test2', [new Value(5)])
+        ]
+      );
+      let facts = new LiteralTreeMap();
+      facts.add(new Functor('test1', [new Value(5)]));
+      facts.add(new Functor('test1', [new Value(3)]));
+
+      let program = [clause1, clause2];
+
+      let resolutor = new Resolutor(program, facts);
+      let newFacts = resolutor.resolve();
+
+      expect(newFacts).to.be.instanceof(LiteralTreeMap);
+      expect(newFacts.size()).to.be.equal(1);
+
+      let array = newFacts.toArray();
+
+      expect(array[0]).to.be.instanceof(Functor);
+      expect(array[0].getId()).to.be.equal('test2/1');
+      expect(array[0].isGround()).to.be.true;
+      expect(array[0].getArguments()[0]).to.be.instanceof(Value);
+      expect(array[0].getArguments()[0].evaluate()).to.be.equal('3');
+    });
   });
 
   describe('reverseQuery', () => {
