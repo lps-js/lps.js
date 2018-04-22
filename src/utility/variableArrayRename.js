add1ca3feced6193b0a85052a6f5b1ca4803a978
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-22 23:37:10 +0100

    update variable rename to resolve renaming

--- src/utility/variableArrayRename.js
+++ src/utility/variableArrayRename.js
@@ -10,6 +10,7 @@ module.exports = function variableArrayRename(varArr, namePatternArg) {
   }
   // create the substitution
   let theta = {};
+
   varArr.forEach((variable) => {
     let varName = variable;
     if (variable instanceof Variable) {
@@ -17,5 +18,24 @@ module.exports = function variableArrayRename(varArr, namePatternArg) {
     }
     theta[varName] = new Variable(namePattern.replace('*', varName));
   });
+
+  let addCount = 0;
+  do {
+    addCount = 0;
+    let newTheta = {};
+    Object.keys(theta).forEach((varName) => {
+      newTheta[varName] = theta[varName];
+      if (theta[varName] instanceof Variable) {
+        let varNameP = theta[varName].evaluate();
+        if (theta[varNameP] !== undefined) {
+          // do another round of renaming
+          newTheta[varName] = new Variable(namePattern.replace('*', varNameP));
+          addCount += 1;
+        }
+        return;
+      }
+    });
+    theta = newTheta;
+  } while (addCount > 0);
   return theta;
 };
