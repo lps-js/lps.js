8564f2dd42969f63a0a916b0858840062eaddca7
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-25 14:13:30 +0100

    update index to export LPS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const LPS = require('./src/LPS');
 const Parser = require('./src/parser/Parser');
 const Engine = require('./src/engine/Engine');
 
-Parser.parseFile('examples/fireSimple.lps')
-  .then((root) => {
-    let engine = new Engine(root);
+LPS.load('examples/towers.lps')
+  .then((engine) => {
     let result = engine.run();
     console.log(JSON.stringify(result));
   });
+
+module.exports = LPS;
