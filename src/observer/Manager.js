4f83dee5d4066ab41b0591e686369c5963addd0a
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-08 00:54:16 +0100

    add implementation of observer pattern manager

---
+++ src/observer/Manager.js
@@ -0,0 +1,27 @@
+function Manager {
+  let _events = {};
+
+  this.addListener = function addListener(event, listener) {
+    if (_events[event] === undefined) {
+      _events[event] = [];
+    }
+
+    _events[event].push(listener);
+  };
+
+  this.clearListeners = function clearListeners(event) {
+    delete _events[event];
+  };
+
+  this.notify = function notify(event, sender) {
+    if (_events[event] === undefined) {
+      return;
+    }
+
+    _events[event].forEach((listener) => {
+      listener(sender);
+    });
+  };
+}
+
+module.exports = Manager;
