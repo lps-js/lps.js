8a2d2a3e82f44d5e7e7044640e927321e23dc638
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-10 16:30:42 +0100

    implement constraint checking

---
+++ src/utility/constraintCheck.js
@@ -0,0 +1,34 @@
+const expandRuleAntecedent = require('./expandRuleAntecedent');
+const Resolutor = require('../engine/Resolutor');
+const Clause = require('../engine/Clause');
+
+module.exports = function constraintCheck(program, builtInFunctorProvider, factsArg, newFacts) {
+  let facts = factsArg;
+  if (!(facts instanceof Array)) {
+    facts = [facts];
+  }
+  facts = facts.concat([newFacts]);
+
+  let result = true;
+  program.forEach((clause) => {
+    if (!result || !clause.isConstraint()) {
+      return;
+    }
+    let expansionResult = [];
+    let literals = clause.getBodyLiterals();
+    expandRuleAntecedent(expansionResult, literals, [], program);
+    expansionResult.forEach((r) => {
+      if (!result) {
+        return;
+      }
+      let clause = new Clause([], r.literalSet);
+      let reductionResult = Resolutor.reduceRuleAntecedent(builtInFunctorProvider, clause, facts);
+      reductionResult.forEach((tuple) => {
+        if (tuple.unresolved.length === 0) {
+          result = false;
+        }
+      });
+    });
+  });
+  return result;
+}
