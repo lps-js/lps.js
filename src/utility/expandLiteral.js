ceb6437291a7ca7c7040ded521702461c19bd813
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-10 10:23:40 +0100

    add implementation of expand literal and expand rule antecedent

---
+++ src/utility/expandLiteral.js
@@ -0,0 +1,47 @@
+const LiteralTreeMap = require('../engine/LiteralTreeMap');
+const Variable = require('../engine/Variable');
+const variableArrayRename = require('./variableArrayRename');
+
+module.exports = function expandLiteral(literal, program, usedVariables) {
+  let literalTreeMap = new LiteralTreeMap();
+  literalTreeMap.add(literal);
+  let renameTheta = variableArrayRename(usedVariables);
+
+  let result = [];
+  program.forEach((clause) => {
+    if (clause.isConstraint()) {
+      return;
+    }
+
+    // assuming horn clauses only
+    let headLiterals = clause.getHeadLiterals();
+    let headLiteral = headLiterals[0].substitute(renameTheta);
+    let unifications = literalTreeMap.unifies(headLiteral);
+    if (unifications.length === 0) {
+      return;
+    }
+
+    unifications.forEach((pair) => {
+      let theta = pair.theta;
+      let outputTheta = {};
+
+      // restore output variables
+      Object.keys(theta)
+        .forEach((varName) => {
+          if (theta[varName] instanceof Variable) {
+            // output variable
+            outputTheta[theta[varName].evaluate()] = new Variable(varName);
+            delete theta[varName];
+          }
+        });
+
+      result.push({
+        clause: clause.getBodyLiterals()
+          .map(l => l.substitute(theta).substitute(renameTheta)),
+        theta: outputTheta,
+        internalTheta: theta
+      });
+    });
+  });
+  return result;
+};
---
+++ src/utility/expandRuleAntecedent.js
@@ -0,0 +1,52 @@
+const expandLiteral = require('./expandLiteral');
+
+module.exports = function expandRuleAntecedent(result, literals, thetaPath, program) {
+  let isLeaf = true;
+
+  let literalsLength = literals.length;
+  let usedVariables = {};
+  literals.forEach((literal) => {
+    literal.getVariables().forEach((v) => {
+      usedVariables[v] = true;
+    });
+  })
+  usedVariables = Object.keys(usedVariables);
+
+  for (let i = 0; i < literalsLength; i += 1) {
+    let literal = literals[i];
+    let otherLiteralsFront = literals.slice(0, i);
+    let otherLiteralsBack = literals.slice(i + 1, literalsLength);
+
+    let expansionResult = expandLiteral(literal, program, usedVariables);
+    if (expansionResult.length > 0) {
+      isLeaf = false;
+    }
+
+    expansionResult.forEach((crrArg) => {
+      // crr needs to rename variables to avoid clashes
+      // also at the same time handle any output variables
+      let remappedClauseFront = otherLiteralsFront.map((l) => {
+        return l
+          .substitute(crrArg.theta);
+      });
+      let remappedClauseBack = otherLiteralsBack.map((l) => {
+        return l
+          .substitute(crrArg.theta);
+      });
+      let newClause = remappedClauseFront
+        .concat(crrArg.clause)
+        .concat(remappedClauseBack);
+      expandRuleAntecedent(result, newClause, thetaPath.concat([crrArg.theta]), program);
+    });
+    if (!isLeaf) {
+      break;
+    }
+  }
+
+  if (isLeaf) {
+    result.push({
+      thetaPath: thetaPath,
+      literalSet: literals
+    });
+  }
+};
