ef3ff5811f0e4623567ee829087ab73444a23327
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-18 23:14:26 +0100

    abstract test function

--- test/programs/index.test.js
+++ test/programs/index.test.js
@@ -4,18 +4,21 @@ const chai = require('chai');
 const expect = chai.expect;
 require('mocha-sinon');
 
+let testFunction = function testFunction(file, done) {
+  LPS.load(__dirname + '/' + file + '.lps')
+    .then((engine) => {
+      return engine.test(__dirname + '/' + file + '.spec.lps');
+    })
+    .then((result) => {
+      expect(result.success).to.be.true;
+      done();
+    });
+}
 
 describe('Programs Test', () => {
   describe('bank-terse', () => {
     it('should pass all expectations', (done) => {
-      LPS.load(__dirname + '/bank-terse.lps')
-        .then((engine) => {
-          return engine.test(__dirname + '/bank-terse.spec.lps');
-        })
-        .then((result) => {
-          expect(result.success).to.be.true;
-          done();
-        });
+      testFunction('bank-terse', done);
     });
   });
 });
