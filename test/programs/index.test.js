8b656c47ff4d0aab2cb0fb22f9d1de9bdea5311f
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-18 23:15:57 +0100

    update test function format

--- test/programs/index.test.js
+++ test/programs/index.test.js
@@ -4,21 +4,21 @@ const chai = require('chai');
 const expect = chai.expect;
 require('mocha-sinon');
 
-let testFunction = function testFunction(file, done) {
-  LPS.load(__dirname + '/' + file + '.lps')
-    .then((engine) => {
-      return engine.test(__dirname + '/' + file + '.spec.lps');
-    })
-    .then((result) => {
-      expect(result.success).to.be.true;
-      done();
-    });
-}
-
-describe('Programs Test', () => {
-  describe('bank-terse', () => {
+let testFunction = function testFunction(file) {
+  describe(file, () => {
     it('should pass all expectations', (done) => {
-      testFunction('bank-terse', done);
+      LPS.load(__dirname + '/' + file + '.lps')
+        .then((engine) => {
+          return engine.test(__dirname + '/' + file + '.spec.lps');
+        })
+        .then((result) => {
+          expect(result.success).to.be.true;
+          done();
+        });
     });
   });
+};
+
+describe('Programs Test', () => {
+  testFunction('bank-terse');
 });
