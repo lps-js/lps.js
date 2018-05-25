32f70b4e27bbb6215292c8a370502221ce5f7c1a
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-25 12:33:37 +0100

    update sync for Parser.parseLiteral test

--- test/parser/Parser.test.js
+++ test/parser/Parser.test.js
@@ -8,20 +8,17 @@ require('mocha-sinon');
 
 describe('Parser', () => {
   describe('parseLiteral', () => {
-    it('should parse and return a literal correctly', (done) => {
+    it('should parse and return a literal correctly', () => {
       let literal = 'test(X, Y)';
-      Parser.parseLiteral(literal)
-        .then((node) => {
-          expect(node).to.be.instanceof(AstNode);
-          expect(node.getType()).to.be.equal(NodeTypes.Functor);
-          let children = node.getChildren();
-          expect(children).to.be.length(2);
-          expect(children[0]).to.be.instanceof(AstNode);
-          expect(children[0].getType()).to.be.equal(NodeTypes.Variable);
-          expect(children[1]).to.be.instanceof(AstNode);
-          expect(children[1].getType()).to.be.equal(NodeTypes.Variable);
-          done();
-        });
+      let node = Parser.parseLiteral(literal);
+      expect(node).to.be.instanceof(AstNode);
+      expect(node.getType()).to.be.equal(NodeTypes.Functor);
+      let children = node.getChildren();
+      expect(children).to.be.length(2);
+      expect(children[0]).to.be.instanceof(AstNode);
+      expect(children[0].getType()).to.be.equal(NodeTypes.Variable);
+      expect(children[1]).to.be.instanceof(AstNode);
+      expect(children[1].getType()).to.be.equal(NodeTypes.Variable);
     });
   });
 });
