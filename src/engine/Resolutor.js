25924810ad50abf59f77b3ea97a31ec26c231483
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-25 23:41:55 +0100

    add explain() in resolutor

--- src/engine/Resolutor.js
+++ src/engine/Resolutor.js
@@ -1,7 +1,10 @@
 const BuiltInFunctorProvider = require('./BuiltInFunctorProvider');
 const Functor = require('./Functor');
 const LiteralTreeMap = require('./LiteralTreeMap');
+const variableArrayRename = require('../utility/variableArrayRename');
 const compactTheta = require('../utility/compactTheta');
+const Unifier = require('./Unifier');
+const Variable = require('./Variable');
 
 function Resolutor() {}
 
@@ -45,6 +48,99 @@ Resolutor.findUnifications = function findUnifications(literal, factsArg) {
   }
   return unifications;
 };
+
+Resolutor.explain =
+  function explain(queryArg, builtInFunctorProvider, clauses, factsArg) {
+    let facts = factsArg;
+    if (facts instanceof LiteralTreeMap) {
+      facts = [facts];
+    }
+
+    let query = queryArg;
+    if (query instanceof Functor) {
+      query = [query];
+    }
+
+    let recursiveResolution = function(remainingLiterals, thetaSoFar) {
+      let result = [];
+      if (remainingLiterals.length === 0) {
+        result.push({
+          theta: thetaSoFar
+        });
+        return result;
+      }
+
+      let literal = remainingLiterals[0].substitute(thetaSoFar);
+      let literalThetas = [];
+      if (builtInFunctorProvider.has(literal.getId())) {
+        literalThetas = builtInFunctorProvider.execute(literal);
+      }
+      let substitutedInstances = Resolutor
+        .handleBuiltInFunctorArgumentInLiteral(builtInFunctorProvider, literal);
+      substitutedInstances.forEach((l) => {
+        literalThetas = literalThetas.concat(Resolutor.findUnifications(l, facts));
+      });
+
+      let variablesInUse = {};
+      for (let i = 1; i < remainingLiterals.length; i += 1) {
+        let otherLiteral = remainingLiterals[i];
+        otherLiteral.getVariables().forEach((v) => {
+          variablesInUse[v] = true;
+        });
+      }
+      variablesInUse = Object.keys(variablesInUse);
+
+      clauses.forEach((clause) => {
+        if (clause.isConstraint()) {
+          // skip constraints
+          return;
+        }
+        // horn clause guarantees only one literal
+        let headLiteral = clause.getHeadLiterals()[0];
+        let unificationTheta = Unifier.unifies([[literal, headLiteral]]);
+        if (unificationTheta === null) {
+          return;
+        }
+
+        let bodyLiterals = clause.getBodyLiterals();
+        let renameTheta = variableArrayRename(variablesInUse);
+        bodyLiterals = bodyLiterals.map((blArg) => {
+          let bl = blArg.substitute(unificationTheta);
+          return bl.substitute(renameTheta);
+        });
+        let newTheta = compactTheta(thetaSoFar, unificationTheta);
+        let subResult = recursiveResolution(bodyLiterals, newTheta);
+        literalThetas = literalThetas.concat(subResult);
+      });
+
+      if (literalThetas.length === 0) {
+        return [];
+      }
+
+      let newRemainingLiterals = remainingLiterals.slice(1, remainingLiterals.length);
+
+      literalThetas.forEach((t) => {
+        if (result === null) {
+          return;
+        }
+        let compactedTheta = compactTheta(thetaSoFar, t.theta);
+        Object.keys(compactedTheta).forEach((k) => {
+          if (compactedTheta[k] instanceof Variable) {
+            let otherName = compactedTheta[k].evaluate();
+            if (t.theta[otherName] != undefined) {
+              compactedTheta[k] = t.theta[otherName];
+            }
+          }
+        });
+        let subResult = recursiveResolution(newRemainingLiterals, compactedTheta);
+        result = result.concat(subResult);
+      });
+      return result;
+    };
+
+    let result = recursiveResolution(query, {});
+    return result;
+  };
 
 Resolutor.reduceRuleAntecedent =
   function reduceRuleAntecedent(builtInFunctorProvider, rule, factsArg) {
