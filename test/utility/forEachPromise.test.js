447b6f71e1cf4c799630d1f9dc1ee6797c3a46cd
Sam-Mauris Yong <mauris@hotmail.sg>
2018-05-26 15:47:11 +0100

    add test for forEachPromise

---
+++ test/utility/forEachPromise.test.js
@@ -0,0 +1,20 @@
+const forEachPromise = require('../../src/utility/forEachPromise');
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('forEachPromise', () => {
+  it('should iterate an array correctly', (done) => {
+    let arr = [1, 2, 3, 4, 5];
+    let lastSeenIndex = -1;
+    forEachPromise(arr)
+      .do((item, index) => {
+        expect(item).to.be.equal(arr[index]);
+        lastSeenIndex = index;
+      })
+      .then(() => {
+        expect(lastSeenIndex).to.be.equal(arr.length - 1);
+        done();
+      });
+  });
+});
