41bfa804781300cb991ac438bbbb8c18b57ca246
Sam-Mauris Yong <mauris@hotmail.sg>
2018-06-13 09:16:22 +0100

    add test for FunctorProvider.define()

--- test/engine/FunctorProvider.test.js
+++ test/engine/FunctorProvider.test.js
@@ -51,5 +51,16 @@ describe('FunctorProvider', () => {
         provider.define('camelCase/3', noop);
       }).to.not.throw();
     });
+
+    it('should define the functor as expected', () => {
+      let provider = new FunctorProvider(() => {
+        return [];
+      });
+      provider.define('testingName', (arg) => {});
+      expect(provider.has('testingName/1')).to.be.true;
+
+      provider.define('testingName/2', noop);
+      expect(provider.has('testingName/2')).to.be.true;
+    });
   });
 });
