542448fa5cf0791d3076ae0a177831cc39917287
Sam-Mauris Yong <mauris@hotmail.sg>
2018-06-13 09:27:07 +0100

    add tests for has

--- test/engine/FunctorProvider.test.js
+++ test/engine/FunctorProvider.test.js
@@ -1,4 +1,6 @@
 const FunctorProvider = require('../../src/engine/FunctorProvider');
+const Functor = require('../../src/engine/Functor');
+const Variable = require('../../src/engine/Variable');
 
 const chai = require('chai');
 const expect = chai.expect;
@@ -61,6 +63,31 @@ describe('FunctorProvider', () => {
 
       provider.define('testingName/2', noop);
       expect(provider.has('testingName/2')).to.be.true;
+    });
+  });
+
+  describe('has(name)', () => {
+    it('should return true for built-in functors', () => {
+      let provider = new FunctorProvider(() => {
+        return [];
+      });
+      expect(provider.has('!/1')).to.be.true;
+      expect(provider.has('+/2')).to.be.true;
+      expect(provider.has('=/2')).to.be.true;
+      expect(provider.has('==/2')).to.be.true;
+      expect(provider.has('member/2')).to.be.true;
+      expect(provider.has('floor/1')).to.be.true;
+      expect(provider.has('*/2')).to.be.true;
+      expect(provider.has('-/2')).to.be.true;
+      expect(provider.has('sin/2')).to.be.true;
+      expect(provider.has('cos/2')).to.be.true;
+      expect(provider.has('tan/2')).to.be.true;
+      expect(provider.has('append/2')).to.be.true;
+
+      let literal = new Functor('!', [new Variable('X')]);
+      expect(provider.has(literal)).to.be.true;
+      let literal2 = new Functor('append', [new Variable('X'), new Variable('Y'), new Variable('Z')]);
+      expect(provider.has(literal2)).to.be.true;
     });
   });
 });
