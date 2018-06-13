49be754191b7a9ec409d083f34c26290fd8551bd
Sam-Mauris Yong <mauris@hotmail.sg>
2018-06-13 12:27:22 +0100

    add test for Manager

---
+++ test/observer/Manager.test.js
@@ -0,0 +1,25 @@
+const Manager = require('../../src/observer/Manager');
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Manager', () => {
+  it('should add observers, notify and clear correctly', () => {
+    let manager = new Manager();
+
+    let receivedSender = null;
+    manager.addListener('test', (sender) => {
+      receivedSender = sender;
+    });
+
+    expect(receivedSender).to.be.null;
+    manager.notify('test', manager);
+    expect(receivedSender).to.be.equal(manager);
+
+    receivedSender = null;
+    manager.clearListeners('test');
+    expect(receivedSender).to.be.null;
+    manager.notify('test', manager);
+    expect(receivedSender).to.be.null;
+  });
+});
