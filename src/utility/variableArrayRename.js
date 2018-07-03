a4f013fbeb85808c4edbd81ec085c3908fb5d880
Sam-Mauris Yong <mauris@hotmail.sg>
2018-07-03 11:42:09 +0100

    fix unexpected variable array rename function

--- src/utility/variableArrayRename.js
+++ src/utility/variableArrayRename.js
@@ -3,7 +3,7 @@ const Variable = require('../engine/Variable');
 module.exports = function variableArrayRename(varArr, namePatternArg) {
   let namePattern = namePatternArg;
   if (!namePattern) {
-    namePattern = 'VX*';
+    namePattern = '$_*';
   }
   if (typeof namePattern !== 'string') {
     throw new Error('name pattern for variable array renaming must be a string.');
@@ -19,22 +19,17 @@ module.exports = function variableArrayRename(varArr, namePatternArg) {
     theta[varName] = new Variable(namePattern.replace('*', varName));
   });
 
-  let addCount = 0;
-  do {
-    addCount = 0;
-    let newTheta = {};
-    Object.keys(theta).forEach((varName) => {
-      newTheta[varName] = theta[varName];
-      if (theta[varName] instanceof Variable) {
-        let varNameP = theta[varName].evaluate();
-        if (theta[varNameP] !== undefined) {
-          // do another round of renaming
-          newTheta[varName] = new Variable(namePattern.replace('*', varNameP));
-          addCount += 1;
-        }
+  let newTheta = {};
+  Object.keys(theta).forEach((varName) => {
+    newTheta[varName] = theta[varName];
+    if (theta[varName] instanceof Variable) {
+      let varNameP = theta[varName].evaluate();
+      if (theta[varNameP] !== undefined) {
+        // do another round of renaming
+        newTheta[varName] = new Variable(namePattern.replace('*', varNameP));
       }
-    });
-    theta = newTheta;
-  } while (addCount > 0);
+    }
+  });
+  theta = newTheta;
   return theta;
 };
