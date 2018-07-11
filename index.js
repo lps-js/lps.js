b0c7e920843d9a1b070c4a48bfdbb2815b99a89f
Sam-Mauris Yong <mauris@hotmail.sg>
2018-07-11 11:00:00 +0100

    remove example code from index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 const LPS = require('./src/LPS');
-const Parser = require('./src/parser/Parser');
-const Engine = require('./src/engine/Engine');
-
-LPS.load('examples/towers.lps')
-  .then((engine) => {
-    let result = engine.run();
-    console.log(JSON.stringify(result));
-  });
 
 module.exports = LPS;
