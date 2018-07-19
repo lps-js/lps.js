9e3ea9c2aaabf8e2ffbd41b1a778d4b2d0f3bbec
Sam-Mauris Yong <mauris@hotmail.sg>
2018-07-19 12:19:26 +0100

    remove unused processObservationDeclarations implementation from engine

--- src/engine/Engine.js
+++ src/engine/Engine.js
@@ -158,44 +158,6 @@ function Engine(program) {
     });
   };
 
-  let processObservationDeclarations = function processObservationDeclarations() {
-    let result = program.query(Program.literal('observe(O, ST, ET)'));
-    result.forEach((r) => {
-      if (r.theta.O === undefined || r.theta.ST === undefined || r.theta.ET === undefined) {
-        return;
-      }
-      let observation = r.theta.O;
-      let startTime = r.theta.ST;
-      let endTime = r.theta.ET;
-
-      if (!(startTime instanceof Value)) {
-        throw new Error('Start time given to observe/3 must be a value.');
-      }
-      if (!(endTime instanceof Value)) {
-        throw new Error('End time given to observe/3 must be a value.');
-      }
-      let sTime = startTime.evaluate();
-      let eTime = endTime.evaluate();
-      if (eTime < sTime) {
-        throw new Error('Invalid ordering of time given to observe/3: Start time must come before end time.');
-      }
-      try {
-        observation = actionSyntacticSugarProcessing(observation);
-      } catch (_) {
-        throw new Error('Invalid action value given for observe/3');
-      }
-
-      if (_observations[sTime] === undefined) {
-        _observations[sTime] = [];
-      }
-
-      _observations[sTime].push({
-        action: observation,
-        endTime: eTime
-      });
-    });
-  };
-
   let preProcessRules = function preProcessRules() {
     let newRules = [];
     let rules = program.getRules();
