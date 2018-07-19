c53a4da1f914c055f9518602918d1c0422dd0bca
Sam-Mauris Yong <mauris@hotmail.sg>
2018-07-19 12:17:52 +0100

    add Observation declaration processor implementation

---
+++ src/engine/builtin/Observation.js
@@ -0,0 +1,34 @@
+const Program = require('../../parser/Program');
+const Value = require('../Value');
+
+function Observation() {
+
+}
+
+Observation.processDeclarations = function processDeclarations(engine, program) {
+  let result = program.query(Program.literal('observe(O, ST, ET)'));
+  result.forEach((r) => {
+    if (r.theta.O === undefined || r.theta.ST === undefined || r.theta.ET === undefined) {
+      return;
+    }
+    let observation = r.theta.O;
+    let startTime = r.theta.ST;
+    let endTime = r.theta.ET;
+
+    if (!(startTime instanceof Value)) {
+      throw new Error('Start time given to observe/3 must be a value.');
+    }
+    if (!(endTime instanceof Value)) {
+      throw new Error('End time given to observe/3 must be a value.');
+    }
+    let sTime = startTime.evaluate();
+    let eTime = endTime.evaluate();
+    if (eTime < sTime) {
+      throw new Error('Invalid ordering of time given to observe/3: Start time must come before end time.');
+    }
+
+    engine.scheduleObservation(observation, sTime, eTime);
+  });
+}
+
+module.exports = Observation;
