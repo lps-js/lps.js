27286f46f893f9da0fe9901eefe69e58dad63d8a
Sam-Mauris Yong <mauris@hotmail.sg>
2018-07-25 09:57:00 +0100

    update constraintCheck to use query instead of reduceRuleAntecedent

--- src/utility/constraintCheck.js
+++ src/utility/constraintCheck.js
@@ -12,26 +12,18 @@ module.exports = function constraintCheck(program) {
   let functorProvider = program.getFunctorProvider();
 
   let result = true;
-  program.getClauses().forEach((clause) => {
-    if (!result || !clause.isConstraint()) {
-      return;
-    }
-    let expansionResult = [];
-    let literals = clause.getBodyLiterals();
-    expandRuleAntecedent(expansionResult, literals, [], program);
-    expansionResult.forEach((r) => {
-      if (!result) {
+  program.getClauses()
+    .forEach((clause) => {
+      if (!result || !clause.isConstraint()) {
         return;
       }
-      let newClause = new Clause([], r.literalSet);
-      let reductionResult = Resolutor
-        .reduceRuleAntecedent(functorProvider, newClause, facts);
-      reductionResult.forEach((tuple) => {
-        if (tuple.unresolved.length === 0) {
-          result = false;
-        }
-      });
+      let expansionResult = [];
+      let literals = clause.getBodyLiterals();
+      let queryResult = program.query(literals);
+
+      if (queryResult.length > 0) {
+        result = false;
+      }
     });
-  });
   return result;
 };
