a8b80ce6c8ba71934725992ac339823b95bbff6b
Sam Yong <samyonggit@gmail.com>
2018-08-05 12:12:18 +0100

    update rule preprocessor to new file

--- src/engine/Engine.js
+++ src/engine/Engine.js
@@ -9,8 +9,6 @@ const Variable = lpsRequire('engine/Variable');
 const processRules = lpsRequire('utility/processRules');
 const compactTheta = lpsRequire('utility/compactTheta');
 const EventManager = lpsRequire('observer/Manager');
-const expandRuleAntecedent = lpsRequire('utility/expandRuleAntecedent');
-const variableArrayRename = lpsRequire('utility/variableArrayRename');
 const constraintCheck = lpsRequire('utility/constraintCheck');
 const Tester = lpsRequire('engine/test/Tester');
 const forEachPromise = lpsRequire('utility/forEachPromise');
@@ -18,6 +16,7 @@ const BuiltinLoader = lpsRequire('engine/builtin/BuiltinLoader');
 const SyntacticSugarProcessor = lpsRequire('engine/builtin/SyntacticSugarProcessor');
 const ObserveDeclarationProcessor = lpsRequire('engine/builtin/Observe');
 const goalTreeSorter = lpsRequire('utility/goalTreeSorter');
+const rulePreProcessor = lpsRequire('engine/builtin/RulePreProcessor');
 
 const stringLiterals = lpsRequire('utility/strings');
 
@@ -170,83 +169,6 @@ function Engine(program, workingDirectory) {
     });
   };
 
-  let preProcessRules = function preProcessRules() {
-    let newRules = [];
-    let rules = program.getRules();
-
-    rules.forEach((rule) => {
-      if (rule.getBodyLiteralsCount() === 0) {
-        newRules.push(rule);
-        return;
-      }
-      let antecedent = rule.getBodyLiterals();
-      let ruleResult = [];
-      expandRuleAntecedent(ruleResult, antecedent, [], program);
-      if (ruleResult.length === 0) {
-        // nothing to do for this rule
-        newRules.push(rule);
-        return;
-      }
-      let consequent = rule.getHeadLiterals();
-
-      let antecedentVariables = {};
-      antecedent.forEach((literal) => {
-        literal.getVariables().forEach((vName) => {
-          antecedentVariables[vName] = true;
-        });
-      });
-
-      let commonVariables = {};
-      let consequentVariables = {};
-      consequent.forEach((literal) => {
-        literal.getVariables().forEach((vName) => {
-          if (antecedentVariables[vName]) {
-            commonVariables[vName] = true;
-          }
-          consequentVariables[vName] = true;
-        });
-      });
-
-      ruleResult.forEach((tuple) => {
-        let newAntecedentVariables = {};
-        tuple.literalSet.forEach((literal) => {
-          literal.getVariables().forEach((vName) => {
-            newAntecedentVariables[vName] = true;
-          });
-        });
-
-        let renameSet = [];
-        Object.keys(newAntecedentVariables).forEach((vName) => {
-          if (consequentVariables[vName] !== undefined && commonVariables[vName] === undefined) {
-            renameSet.push(vName);
-          }
-        });
-
-        let tupleConsequent = consequent.concat([]);
-        let replacement = {};
-        Object.keys(commonVariables).forEach((k) => {
-          replacement[k] = new Variable(k);
-        });
-        tuple.thetaPath.forEach((theta) => {
-          Object.keys(replacement).forEach((k) => {
-            if (replacement[k] instanceof Variable) {
-              let vName = replacement[k].evaluate();
-              if (theta[vName] !== undefined) {
-                replacement[k] = theta[vName];
-              } else if (replacement[vName] !== undefined) {
-                replacement[k] = replacement[vName];
-              }
-            }
-          });
-        });
-        let renameTheta = variableArrayRename(renameSet);
-        tupleConsequent = tupleConsequent.map(literal => literal.substitute(replacement).substitute(renameTheta));
-        newRules.push(new Clause(tupleConsequent, tuple.literalSet));
-      });
-    });
-    program.updateRules(newRules);
-  };
-
   let processCycleObservations = function processCycleObservations() {
     let observationTerminated = [];
     let observationInitiated = [];
@@ -317,7 +239,6 @@ function Engine(program, workingDirectory) {
       let initiatingFluent;
       switch (type) {
         case 'update':
-          // console.log('adding update ' + action + ' for ' + oldFluent + ' to ' + newFluent);
           terminatingFluent = SyntacticSugarProcessor.fluent(oldFluent, lastArgument);
           initiatingFluent = SyntacticSugarProcessor.fluent(newFluent, lastArgument);
           fluentActors.push({
@@ -328,7 +249,6 @@ function Engine(program, workingDirectory) {
           });
           break;
         case 'initiate':
-          // console.log('adding initiate ' + action + ' for ' + newFluent);
           initiatingFluent = SyntacticSugarProcessor.fluent(newFluent, lastArgument);
           fluentActors.push({
             action: action,
@@ -929,7 +849,7 @@ function Engine(program, workingDirectory) {
     processEventDeclarations();
     processInitialFluentDeclarations();
     ObserveDeclarationProcessor.processDeclarations(this, program);
-    preProcessRules();
+    rulePreProcessor(this, program);
     _engineEventManager.notify('ready', this);
   });
 
---
+++ src/engine/builtin/RulePreProcessor.js
@@ -0,0 +1,82 @@
+const Clause = lpsRequire('engine/Clause');
+const variableArrayRename = lpsRequire('utility/variableArrayRename');
+const expandRuleAntecedent = lpsRequire('utility/expandRuleAntecedent');
+
+let rulePreProcessor = function rulePreProcessor(engine, program) {
+  let newRules = [];
+  let rules = program.getRules();
+
+  rules.forEach((rule) => {
+    if (rule.getBodyLiteralsCount() === 0) {
+      newRules.push(rule);
+      return;
+    }
+    let antecedent = rule.getBodyLiterals();
+    let ruleResult = [];
+    expandRuleAntecedent(ruleResult, antecedent, [], program);
+    if (ruleResult.length === 0) {
+      // nothing to do for this rule
+      newRules.push(rule);
+      return;
+    }
+    let consequent = rule.getHeadLiterals();
+
+    let antecedentVariables = {};
+    antecedent.forEach((literal) => {
+      literal.getVariables().forEach((vName) => {
+        antecedentVariables[vName] = true;
+      });
+    });
+
+    let commonVariables = {};
+    let consequentVariables = {};
+    consequent.forEach((literal) => {
+      literal.getVariables().forEach((vName) => {
+        if (antecedentVariables[vName]) {
+          commonVariables[vName] = true;
+        }
+        consequentVariables[vName] = true;
+      });
+    });
+
+    ruleResult.forEach((tuple) => {
+      let newAntecedentVariables = {};
+      tuple.literalSet.forEach((literal) => {
+        literal.getVariables().forEach((vName) => {
+          newAntecedentVariables[vName] = true;
+        });
+      });
+
+      let renameSet = [];
+      Object.keys(newAntecedentVariables).forEach((vName) => {
+        if (consequentVariables[vName] !== undefined && commonVariables[vName] === undefined) {
+          renameSet.push(vName);
+        }
+      });
+
+      let tupleConsequent = consequent.concat([]);
+      let replacement = {};
+      Object.keys(commonVariables).forEach((k) => {
+        replacement[k] = new Variable(k);
+      });
+      tuple.thetaPath.forEach((theta) => {
+        Object.keys(replacement).forEach((k) => {
+          if (replacement[k] instanceof Variable) {
+            let vName = replacement[k].evaluate();
+            if (theta[vName] !== undefined) {
+              replacement[k] = theta[vName];
+            } else if (replacement[vName] !== undefined) {
+              replacement[k] = replacement[vName];
+            }
+          }
+        });
+      });
+      let renameTheta = variableArrayRename(renameSet);
+      tupleConsequent = tupleConsequent.map(literal => literal.substitute(replacement).substitute(renameTheta));
+      newRules.push(new Clause(tupleConsequent, tuple.literalSet));
+    });
+  });
+  program.updateRules(newRules);
+};
+
+module.exports = rulePreProcessor;
