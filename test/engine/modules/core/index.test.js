656f7dafadf1b04a8bf4aa161210539a345dcb63
Sam Yong <samyonggit@gmail.com>
2018-08-07 10:03:29 +0100

    add more test for coreModule

--- test/engine/modules/core/index.test.js
+++ test/engine/modules/core/index.test.js
@@ -11,7 +11,14 @@ const expect = chai.expect;
 
 describe('coreModule', () => {
   describe('!/1', () => {
-    it('return correct negation result', () => {
+    it('should be defined', () => {
+      let program = new Program();
+      coreModule(null, program);
+      let functorProvider = program.getFunctorProvider();
+      expect(functorProvider.has('!/1')).to.be.true;
+    });
+
+    it('should return correct negation result', () => {
       let program = new Program();
       let engine = new Engine(program);
 
@@ -23,7 +30,7 @@ describe('coreModule', () => {
       expect(result[0].theta).to.be.empty;
     });
 
-    it('return correct negation result', () => {
+    it('should return correct negation result', () => {
       let facts = new LiteralTreeMap();
       let program = new Program();
       let engine = new Engine(program);
@@ -35,6 +42,138 @@ describe('coreModule', () => {
       let result = program.query(Program.literal('!fact(a)'));
       expect(result).to.be.an('array');
       expect(result).to.be.length(0);
+    });
+  });
+
+  describe('+/2', () => {
+    let program;
+    beforeEach(() => {
+      program = new Program();
+
+      // core module is loaded by Engine
+      coreModule(null, program);
+    });
+
+    it('should be defined', () => {
+      let functorProvider = program.getFunctorProvider();
+      expect(functorProvider.has('+/2')).to.be.true;
+    });
+
+    it('should return addition correctly', () => {
+      let functorProvider = program.getFunctorProvider();
+      let params = [
+        new Value(1),
+        new Value(2)
+      ]
+      let result = functorProvider.execute(new Functor('+', params));
+      expect(result).to.be.an('array');
+      expect(result).to.be.length(1);
+      expect(result[0]).to.have.property('theta');
+      expect(result[0].theta).to.be.an('object');
+      expect(result[0].theta).to.be.empty;
+      expect(result[0]).to.have.property('replacement');
+      expect(result[0].replacement).to.be.an('object');
+      expect(result[0].replacement).to.be.instanceof(Value);
+      expect(result[0].replacement.evaluate()).to.be.equal(3);
+    });
+  });
+
+  describe('-/2', () => {
+    let program;
+    beforeEach(() => {
+      program = new Program();
+
+      // core module is loaded by Engine
+      coreModule(null, program);
+    });
+
+    it('should be defined', () => {
+      let functorProvider = program.getFunctorProvider();
+      expect(functorProvider.has('-/2')).to.be.true;
+    });
+
+    it('should return subtraction correctly', () => {
+      let functorProvider = program.getFunctorProvider();
+      let params = [
+        new Value(2),
+        new Value(1)
+      ]
+      let result = functorProvider.execute(new Functor('-', params));
+      expect(result).to.be.an('array');
+      expect(result).to.be.length(1);
+      expect(result[0]).to.have.property('theta');
+      expect(result[0].theta).to.be.an('object');
+      expect(result[0].theta).to.be.empty;
+      expect(result[0]).to.have.property('replacement');
+      expect(result[0].replacement).to.be.an('object');
+      expect(result[0].replacement).to.be.instanceof(Value);
+      expect(result[0].replacement.evaluate()).to.be.equal(1);
+    });
+  });
+
+  describe('*/2', () => {
+    let program;
+    beforeEach(() => {
+      program = new Program();
+
+      // core module is loaded by Engine
+      coreModule(null, program);
+    });
+
+    it('should be defined', () => {
+      let functorProvider = program.getFunctorProvider();
+      expect(functorProvider.has('*/2')).to.be.true;
+    });
+
+    it('should return multiplication correctly', () => {
+      let functorProvider = program.getFunctorProvider();
+      let params = [
+        new Value(2),
+        new Value(3)
+      ]
+      let result = functorProvider.execute(new Functor('*', params));
+      expect(result).to.be.an('array');
+      expect(result).to.be.length(1);
+      expect(result[0]).to.have.property('theta');
+      expect(result[0].theta).to.be.an('object');
+      expect(result[0].theta).to.be.empty;
+      expect(result[0]).to.have.property('replacement');
+      expect(result[0].replacement).to.be.an('object');
+      expect(result[0].replacement).to.be.instanceof(Value);
+      expect(result[0].replacement.evaluate()).to.be.equal(6);
+    });
+  });
+
+  describe('//2', () => {
+    let program;
+    beforeEach(() => {
+      program = new Program();
+
+      // core module is loaded by Engine
+      coreModule(null, program);
+    });
+
+    it('should be defined', () => {
+      let functorProvider = program.getFunctorProvider();
+      expect(functorProvider.has('//2')).to.be.true;
+    });
+
+    it('should return multiplication correctly', () => {
+      let functorProvider = program.getFunctorProvider();
+      let params = [
+        new Value(6),
+        new Value(2)
+      ]
+      let result = functorProvider.execute(new Functor('/', params));
+      expect(result).to.be.an('array');
+      expect(result).to.be.length(1);
+      expect(result[0]).to.have.property('theta');
+      expect(result[0].theta).to.be.an('object');
+      expect(result[0].theta).to.be.empty;
+      expect(result[0]).to.have.property('replacement');
+      expect(result[0].replacement).to.be.an('object');
+      expect(result[0].replacement).to.be.instanceof(Value);
+      expect(result[0].replacement.evaluate()).to.be.equal(3);
     });
   });
 });
