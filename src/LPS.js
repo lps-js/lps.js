1fdc1dcf8f3b3ba3acbbcc68588cc171b8581384
Sam Yong <samyonggit@gmail.com>
2018-08-08 18:06:52 +0100

    change load to loadFile in LPS and add loadString

--- src/LPS.js
+++ src/LPS.js
@@ -1,12 +1,16 @@
-global.lpsRequire = name => require(`${__dirname}/${name}`);
+const path = require('path');
+
+global.lpsRequire = (name) => {
+  // let pathname = path.join(__dirname, name);
+  return require(`${__dirname}/${name}`);
+};
+
 const Program = lpsRequire('parser/Program');
 const Engine = lpsRequire('engine/Engine');
 const Value = lpsRequire('engine/Value');
 const Variable = lpsRequire('engine/Variable');
 const Functor = lpsRequire('engine/Functor');
 const List = lpsRequire('engine/List');
-
-const path = require('path');
 
 function LPS() {
 
@@ -20,8 +24,23 @@ LPS.literalSet = function literalSet(str) {
   return Program.literalSet(str);
 };
 
-LPS.load = function load(file) {
+LPS.loadString = function loadString(source) {
+  return Program.fromString(source)
+    .then((program) => {
+      return new Promise((resolve) => {
+        let engine = new Engine(program);
+        engine.on('ready', () => {
+          resolve(engine);
+        });
+      });
+    });
+}
+
+LPS.loadFile = function loadFile(file) {
   // TODO: consider browser context
+  if (process.browser) {
+    throw new Error('Cannot load file in browser mode');
+  }
   return Program.fromFile(file)
     .then((program) => {
       return new Promise((resolve) => {
