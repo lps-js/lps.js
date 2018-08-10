a45048644e1849b77e6674f5134327fe5d69f56c
Sam Yong <samyonggit@gmail.com>
2018-08-10 11:06:02 +0100

    update sortTimables to analyse variable orderin

--- src/utility/sortTimables.js
+++ src/utility/sortTimables.js
@@ -1,32 +1,62 @@
+const Variable = lpsRequire('engine/Variable');
+const Value = lpsRequire('engine/Value');
+
 function sortTimables(conjunction, forTime) {
   let earlyConjuncts = [];
   let laterConjuncts = [];
 
-  conjunction.forEach((conjunct) => {
+  let dependentTimeVariables = {};
+
+  for (let k = 0; k < conjunction.length; k += 1) {
+    let conjunct = conjunction[k];
+    let conjunctStartTime = conjunct.getStartTime();
+    let conjunctEndTime = conjunct.getEndTime();
+
+    if (conjunctEndTime instanceof Variable) {
+      let endTimeName = conjunctEndTime.evaluate();
+      if (conjunctStartTime instanceof Value
+          || (conjunctStartTime instanceof Variable
+            && conjunctStartTime.evaluate() !== endTimeName)) {
+        // different start/end times
+        dependentTimeVariables[endTimeName] = true;
+      }
+    }
+  }
+
+  for (let k = 0; k < conjunction.length; k += 1) {
+    let conjunct = conjunction[k];
     if (!conjunct.isInRange(forTime)) {
       laterConjuncts.push(conjunct);
-      return;
+      continue;
     }
 
-    if (earlyConjuncts.length === 0) {
-      earlyConjuncts.push(conjunct);
-      return;
-    }
-
+    let movedToLater = [];
     for (let i = 0; i < earlyConjuncts; i += 1) {
       if (conjunct.isEarlierThan(earlyConjuncts[i])) {
         laterConjuncts = laterConjuncts.concat(earlyConjuncts);
         earlyConjuncts = [conjunct];
-        return;
       }
     }
 
-    if (conjunct.isLaterThan(earlyConjuncts[0])) {
-      laterConjuncts.push(conjunct);
-      return;
+    let conjunctStartTime = conjunct.getStartTime();
+
+    if (conjunctStartTime instanceof Variable) {
+      if(dependentTimeVariables[conjunctStartTime.evaluate()] !== undefined) {
+        laterConjuncts.push(conjunct);
+        continue;
+      }
+
+      // let conjunctEndTime = conjunct.getEndTime();
+      // if (conjunctEndTime instanceof Variable) {
+      //   let endTimeName = conjunctEndTime.evaluate();
+      //   if (conjunctStartTime.evaluate() !== endTimeName) {
+      //     // different start/end times
+      //     dependentTimeVariables[endTimeName] = true;
+      //   }
+      // }
     }
     earlyConjuncts.push(conjunct);
-  });
+  }
 
   return [
     earlyConjuncts,
