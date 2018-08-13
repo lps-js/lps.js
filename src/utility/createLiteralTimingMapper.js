34ff50d7345e618c25127eeeaa21f3e3f66e8dea
Sam Yong <samyonggit@gmail.com>
2018-08-13 16:01:08 +0100

    add implementation of createLiteralTimingMapper

---
+++ src/utility/createLiteralTimingMapper.js
@@ -0,0 +1,53 @@
+const Timable = lpsRequire('engine/Timable');
+const Functor = lpsRequire('engine/Functor');
+const Variable = lpsRequire('engine/Variable');
+
+const createLiteralTimingMapper = function createLiteralTimingMapper(program) {
+  let lastTiming = null;
+  return (_literal) => {
+    let literal = _literal;
+    if (literal instanceof Timable) {
+      return literal;
+    }
+
+    let isNegated = false;
+    while (literal instanceof Functor && literal.getId() === '!/1') {
+      isNegated = !isNegated;
+      literal = literal.getArguments()[0];
+    }
+
+    let literalArgs = literal.getArguments();
+    let goal;
+    if (program.isFluent(literal.getName() + '/' + (literal.getArgumentCount()-1))) {
+      let timing = literalArgs[literalArgs.length - 1];
+      // take out the timing argument
+      literalArgs = literalArgs.slice(0, literalArgs.length - 1);
+      goal = new Functor(literal.getName(), literalArgs);
+      if (isNegated) {
+        goal = new Functor('!', [goal]);
+      }
+      return new Timable(goal, timing, timing);
+    }
+
+    let actionName = literal.getName() + '/' + (literal.getArgumentCount()-2);
+    if (program.isAction(actionName) || program.isEvent(actionName)) {
+      let startTime = literalArgs[literalArgs.length - 2];
+      let endTime = literalArgs[literalArgs.length - 1];
+      // take out the timing arguments
+      literalArgs = literalArgs.slice(0, literalArgs.length - 2);
+      goal = new Functor(literal.getName(), literalArgs);
+      if (isNegated) {
+        goal = new Functor('!', [goal]);
+      }
+      return new Timable(goal, startTime, endTime);
+    }
+
+    goal = literal;
+    if (isNegated) {
+      goal = new Functor('!', [goal]);
+    }
+    return goal;
+  };
+};
+
+module.exports = createLiteralTimingMapper;
