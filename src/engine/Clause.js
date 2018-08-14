49d8e7819d535ac61270a7380437fe98214a7524
Sam Yong <samyonggit@gmail.com>
2018-08-14 22:07:25 +0100

    add clause comments

--- src/engine/Clause.js
+++ src/engine/Clause.js
@@ -1,8 +1,18 @@
+/**
+ * A generic clause representation
+ * @param       {Array} head The head literals of the clause
+ * @param       {Array} body The body literals of the clause
+ * @constructor
+ */
 function Clause(head, body) {
   // array of expressions
   let _head = head;
   let _body = body;
 
+  /**
+   * Get all variables that occur in this clause
+   * @return {Array} Return an array of unique variables that occur in this clause
+   */
   this.getVariables = function getVariables() {
     let hash = {};
 
@@ -18,62 +28,85 @@ function Clause(head, body) {
     return Object.keys(hash);
   };
 
-  this.isConstraint = function isQuery() {
+  /**
+   * Determine if the clause is a constraint clause
+   * @return {Boolean} Return true if the clause is a constraint (i.e. no head literals),
+   *    false otherwise.
+   */
+  this.isConstraint = function isConstraint() {
     return _head.length === 0;
   };
 
-  this.isEmpty = function isEmpty() {
-    return _head.length === 0 && _body.length === 0;
-  };
-
+  /**
+   * Get the number of head literals in this clause
+   * @return {number} Return the number of head literals
+   */
   this.getHeadLiteralsCount = function getHeadLiteralsCount() {
     return _head.length;
   };
 
+  /**
+   * Get the number of body literals in this clause
+   * @return {number} Return the number of body literals
+   */
   this.getBodyLiteralsCount = function getBodyLiteralsCount() {
     return _body.length;
   };
 
+  /**
+   * Determine if the clause is ground or not
+   * @return {Boolean} Return true if the clause is ground, false otherwise.
+   */
   this.isGround = function isGround() {
-    let result = true;
-
     for (let i = 0; i < _head.length; i += 1) {
       let literal = _head[i];
       if (!literal.isGround()) {
-        result = false;
-        break;
+        return false;
       }
-    }
-
-    if (!result) {
-      return result;
     }
 
     for (let i = 0; i < _body.length; i += 1) {
       let literal = _body[i];
       if (!literal.isGround()) {
-        result = false;
-        break;
+        return false;
       }
     }
 
-    return result;
+    return true;
   };
 
+  /**
+   * Perform substitution on all terms in the clause
+   * and return a substituted copy of the clause.
+   * @param  {Object} theta The substitution theta
+   * @return {Clause}       Return a copy of the substituted clause
+   */
   this.substitute = function substitute(theta) {
     let newHead = _head.map(expressions => expressions.substitute(theta));
     let newBody = _body.map(expressions => expressions.substitute(theta));
     return new Clause(newHead, newBody);
   };
 
+  /**
+   * Get the head literals in the clause
+   * @return {Array} Return a shallow copy of the head literals in the clause
+   */
   this.getHeadLiterals = function getHeadLiterals() {
-    return [].concat(_head);
+    return _head.concat([]);
   };
 
+  /**
+   * Get the body literals in the clause
+   * @return {Array} Return a shallow copy of the body literals in the clause
+   */
   this.getBodyLiterals = function getBodyLiterals() {
-    return [].concat(_body);
+    return _body.concat([]);
   };
 
+  /**
+   * Get a string representation of the clause
+   * @return {string} Return a string representation of the clause
+   */
   this.toString = function toString() {
     let result = '';
     for (let i = 0; i < _head.length; i += 1) {
