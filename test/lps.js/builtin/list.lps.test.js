4c14dfe88bf350cdc794887f69108f8cfcbaa6ea
Sam Yong <samyonggit@gmail.com>
2018-08-16 15:55:27 +0100

    add test for min_list/3 and sum_list/3

--- test/lps.js/builtin/list.lps.test.js
+++ test/lps.js/builtin/list.lps.test.js
@@ -62,4 +62,99 @@ describe('list.lps', () => {
       expect(result).to.have.length(0);
     });
   });
+
+  describe('min_list/3', () => {
+    it('should return correct value for list of 2 elements', () => {
+      let result = engine.query(Program.literal('min_list([5, 8], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(5);
+    });
+
+    it('should return correct value for list of more elements', () => {
+      let result = engine.query(Program.literal('min_list([8, 4, 6, 10, 9], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(4);
+    });
+
+    it('should return correct value for list of only 1 element', () => {
+      let result = engine.query(Program.literal('min_list([8], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(8);
+    });
+
+    it('should return no result for list of no elements', () => {
+      let result = engine.query(Program.literal('min_list([], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(0);
+    });
+  });
+
+  describe('sum_list/3', () => {
+    it('should return correct value for list of 2 elements', () => {
+      let result = engine.query(Program.literal('sum_list([5, 8], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(13);
+    });
+
+    it('should return correct value for list of more elements', () => {
+      let result = engine.query(Program.literal('sum_list([8, 4, 6, 10, 9], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(37);
+    });
+
+    it('should return correct value for list of only 1 element', () => {
+      let result = engine.query(Program.literal('sum_list([8], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(8);
+    });
+
+    it('should return no result for list of no elements', () => {
+      let result = engine.query(Program.literal('sum_list([], A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(0);
+    });
+  });
 });
