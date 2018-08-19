452016de680aa4d7b7401b00f40fdab1e3ee4b5b
Sam Yong <samyonggit@gmail.com>
2018-08-19 16:58:27 +0100

    update error message to show lines before and after

--- src/parser/unexpectedTokenErrorMessage.js
+++ src/parser/unexpectedTokenErrorMessage.js
@@ -7,32 +7,72 @@ const stringLiterals = lpsRequire('utility/strings');
 
 const NEWLINE_CHAR = '\n';
 
-module.exports = function unexpectedTokenErrorMessage(source, currentToken, likelyMissing) {
-  let lineIndex = currentToken.line;
-  let sourceLength = source.length;
+const getLines = function getLines(source, lineNumbersArg) {
+  let lineNumbers = lineNumbersArg.concat([]);
+  lineNumbers.sort();
 
   let currentLineIndex = 0;
-  let sourcePointerIndex = 0;
+  let sourcePointerIndex = -1;
+  let sourceLength = source.length;
+
   let lineStartIndex = -1;
   let lineEndIndex = -1;
-  // extract the line where the error occurred
+
+  let result = [];
+
   while (sourcePointerIndex < sourceLength) {
-    if (source[sourcePointerIndex] === NEWLINE_CHAR) {
-      currentLineIndex += 1;
-      if (currentLineIndex === lineIndex) {
-        lineStartIndex = sourcePointerIndex + 1;
-      } else if (currentLineIndex === lineIndex + 1) {
-        lineEndIndex = sourcePointerIndex;
+    sourcePointerIndex += 1;
+    if (source[sourcePointerIndex] !== NEWLINE_CHAR) {
+      continue;
+    }
+    // new line character
+    currentLineIndex += 1;
+    if (currentLineIndex === lineNumbers[0] + 1) {
+      lineEndIndex = sourcePointerIndex;
+      result.push(source.substring(lineStartIndex + 1, lineEndIndex));
+      lineNumbers.shift();
+      lineStartIndex = -1;
+      lineEndIndex = -1;
+      if (lineNumbers.length === 0) {
+        break;
       }
     }
-    sourcePointerIndex += 1;
+
+    if (currentLineIndex === lineNumbers[0]) {
+      lineStartIndex = sourcePointerIndex;
+    }
   }
-  if (lineEndIndex === -1) {
+  if (lineStartIndex > -1 && lineEndIndex === -1) {
     // last line
     lineEndIndex = sourceLength - 1;
+    result.push(source.substring(lineStartIndex + 1, lineEndIndex));
   }
+  return result;
+};
 
-  let line = source.substring(lineStartIndex, lineEndIndex);
+module.exports = function unexpectedTokenErrorMessage(source, currentToken, likelyMissing) {
+  let lineIndex = currentToken.line;
+
+  let numLines = source.split(NEWLINE_CHAR).length;
+  // extract the line where the error occurred
+  let lineNumbers = [];
+  if (lineIndex > 0) {
+    lineNumbers.push(lineIndex - 1);
+  }
+  lineNumbers.push(lineIndex);
+  if (lineIndex < numLines - 1) {
+    lineNumbers.push(lineIndex + 1);
+  }
+  let lines = getLines(source, lineNumbers);
+  let lineOutput = '';
+  lines.forEach((line, idx) => {
+    // console.log('"'+line+'"')
+    lineOutput += '\t' + (lineNumbers[idx] + 1) + ' | ' + line + '\n';
+    if (lineNumbers[idx] === lineIndex) {
+      lineOutput += '\t    ' + ' '.repeat(currentToken.col) + '^\n';
+    }
+  });
+
   let tokenType = String(currentToken.type).slice(7, -1) || 'undefined';
 
   let message = stringLiterals(
@@ -43,8 +83,7 @@ module.exports = function unexpectedTokenErrorMessage(source, currentToken, like
     currentToken.col + 1,
     currentToken.file,
     currentToken.line + 1,
-    line,
-    ' '.repeat(currentToken.col) + '^'
+    lineOutput
   );
   if (likelyMissing !== undefined) {
     message += stringLiterals('parser.likelyMissingInfo', likelyMissing);
