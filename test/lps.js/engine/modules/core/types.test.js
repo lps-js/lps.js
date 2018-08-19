e48aec9cb7d72a889a334414a2119a42985f224b
Sam Yong <samyonggit@gmail.com>
2018-08-19 17:04:02 +0100

    add test for is_number/1

--- test/lps.js/engine/modules/core/types.test.js
+++ test/lps.js/engine/modules/core/types.test.js
@@ -51,7 +51,7 @@ describe('coreModule', () => {
         expect(result).to.be.an('array');
         expect(result).to.be.length(0);
       });
-    });
+    }); // is_ground/1
 
     describe('is_list/1', () => {
       let program;
@@ -89,7 +89,7 @@ describe('coreModule', () => {
         expect(result).to.be.an('array');
         expect(result).to.be.length(0);
       });
-    });
+    }); // is_list/1
 
     describe('is_variable/1', () => {
       let program;
@@ -127,6 +127,57 @@ describe('coreModule', () => {
         expect(result).to.be.an('array');
         expect(result).to.be.length(0);
       });
-    });
+    }); // is_variable/1
+
+    describe('is_number/1', () => {
+      let program;
+      beforeEach(() => {
+        program = new Program();
+
+        // core module is loaded by Engine
+        coreModule(null, program);
+      });
+
+      it('should be defined', () => {
+        let functorProvider = program.getFunctorProvider();
+        expect(functorProvider.has('is_number/1')).to.be.true;
+      });
+
+      it('should return is_number() correctly for an integer number value', () => {
+        let functorProvider = program.getFunctorProvider();
+        let params = [
+          new Value(5)
+        ];
+        let result = functorProvider.execute(new Functor('is_number', params));
+        expect(result).to.be.an('array');
+        expect(result).to.be.length(1);
+        expect(result[0]).to.have.property('theta');
+        expect(result[0].theta).to.be.an('object');
+        expect(result[0].theta).to.be.empty;
+      });
+
+      it('should return is_number() correctly for a float number value', () => {
+        let functorProvider = program.getFunctorProvider();
+        let params = [
+          new Value(0.885)
+        ];
+        let result = functorProvider.execute(new Functor('is_number', params));
+        expect(result).to.be.an('array');
+        expect(result).to.be.length(1);
+        expect(result[0]).to.have.property('theta');
+        expect(result[0].theta).to.be.an('object');
+        expect(result[0].theta).to.be.empty;
+      });
+
+      it('should return is_number() correctly for a non-number value', () => {
+        let functorProvider = program.getFunctorProvider();
+        let params = [
+          new Value('test')
+        ];
+        let result = functorProvider.execute(new Functor('is_number', params));
+        expect(result).to.be.an('array');
+        expect(result).to.be.length(0);
+      });
+    }); // is_number/1
   });
 });
