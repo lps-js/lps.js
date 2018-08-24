ff87ca06f44941ab9283ae9115523ac7493d4b72
Sam Yong <samyonggit@gmail.com>
2018-08-24 13:40:46 +0100

    update comments

--- src/lpsRequire.js
+++ src/lpsRequire.js
@@ -1,4 +1,8 @@
+/*
+  This file is part of the lps.js project, released open source under
+  the BSD 3-Clause license. For more info, please see https://github.com/mauris/lps.js
+ */
+
 module.exports = function lpsRequire(name) {
-  // let pathname = path.join(__dirname, name);
   return require(`${__dirname}/${name}`);
 };
