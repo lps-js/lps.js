9d8bc69583c3170e6e88d9ea96319e0f4cec5d5f
Sam Yong <samyonggit@gmail.com>
2018-08-27 19:20:14 +0100

    update types.lps test

--- test/lps.js/builtin/types.lps.test.js
+++ test/lps.js/builtin/types.lps.test.js
@@ -24,7 +24,8 @@ describe('types.lps', () => {
   });
 
   describe('atom_number/2', () => {
-    it('should return correct value for list of 2 elements', () => {
+    it('should return correct value converting a string representation of number to number', () => {
+      let result = engine.query(ProgramFactory.literal('atom_number(\'5\', A)'));
 
       expect(result).to.be.an('array');
       expect(result).to.have.length(1);
@@ -33,6 +34,39 @@ describe('types.lps', () => {
       expect(result[0].theta).to.have.property('A');
       expect(result[0].theta.A).to.be.instanceof(Value);
       expect(result[0].theta.A.evaluate()).to.be.equal(5);
+    });
+
+    it('should return no result for a NaN conversion.', () => {
+      let result = engine.query(ProgramFactory.literal('atom_number(\'a\', A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(0);
+    });
+  });
+
+  describe('atom_string/2', () => {
+    it('should return correct value converting a string representation of number to number', () => {
+      let result = engine.query(ProgramFactory.literal('atom_string(\'100\', A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal('100');
+    });
+
+    it('should return correct result for a string to string conversion.', () => {
+      let result = engine.query(ProgramFactory.literal('atom_string(\'a\', A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal('a');
     });
   });
 });
