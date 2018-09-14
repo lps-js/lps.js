3121d5e25e6ccb9b8ac8e9584871fd0e85e6d4aa
Sam Yong <samyonggit@gmail.com>
2018-09-15 00:14:59 +0100

    add ceil/2 test

--- test/lps.js/builtin/math.lps.test.js
+++ test/lps.js/builtin/math.lps.test.js
@@ -528,6 +528,35 @@ describe('math.lps', () => {
     });
   }); // floor/2
 
+  describe('ceil/2', () => {
+    it('should return correct result for variable replacement', () => {
+      let result = engine.query(ProgramFactory.literal('ceil(2.6, A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(Math.ceil(2.6));
+    });
+
+    it('should return correct result for matching output', () => {
+      let result = engine.query(ProgramFactory.literal('ceil(2.6, ' + Math.ceil(2.6) + ')'));
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+      expect(result[0].theta).to.be.empty;
+    });
+
+    it('should return empty array for non-match', () => {
+      let result = engine.query(ProgramFactory.literal('ceil(2.6, 2)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(0);
+    });
+  }); // ceil/2
+
   describe('succ/2', () => {
     it('should return correct result for variable replacement 1', () => {
       let result = engine.query(ProgramFactory.literal('succ(5, A)'));
