8a0f56900406206243eba732930942f5cf904b84
Sam Yong <samyonggit@gmail.com>
2018-09-14 22:33:13 +0100

    add Cut node type

--- src/parser/NodeTypes.js
+++ src/parser/NodeTypes.js
@@ -14,6 +14,7 @@ module.exports = {
   ListHead: Symbol('ListHead'),
   List: Symbol('List'),
   Negation: Symbol('Negation'),
+  Cut: Symbol('Cut'),
   Timable: Symbol('Timable'),
   BinaryOperator: Symbol('BinaryOperator'),
   UnaryOperator: Symbol('UnaryOperator'),
