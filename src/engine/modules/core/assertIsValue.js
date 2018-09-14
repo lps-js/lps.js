5477335961b9b62f1afa15bf5cdab2cf9572ad5a
Sam Yong <samyonggit@gmail.com>
2018-09-14 20:26:39 +0100

    value check

--- src/engine/modules/core/assertIsValue.js
+++ src/engine/modules/core/assertIsValue.js
@@ -8,7 +8,8 @@ const Value = lpsRequire('engine/Value');
 const Functor = lpsRequire('engine/Functor');
 
 module.exports = function assertIsValue(val) {
-  if (!(val instanceof Value) && !(val instanceof Functor)) {
+  if (!(val instanceof Value)
+      && !(val instanceof Functor && val.getArgumentCount() === 0)) {
     throw new Error('Must be value, ' + val + ' given');
   }
 };
