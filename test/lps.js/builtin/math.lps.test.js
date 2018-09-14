af65fe0cf11a572ef2a79bb5b91b149c03974782
Sam Yong <samyonggit@gmail.com>
2018-09-15 00:12:31 +0100

    add test for log/2

--- test/lps.js/builtin/math.lps.test.js
+++ test/lps.js/builtin/math.lps.test.js
@@ -441,6 +441,35 @@ describe('math.lps', () => {
     });
   }); // exp/2
 
+  describe('log/2', () => {
+    it('should return correct result for variable replacement', () => {
+      let result = engine.query(ProgramFactory.literal('log(25, A)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+
+      expect(result[0].theta).to.have.property('A');
+      expect(result[0].theta.A).to.be.instanceof(Value);
+      expect(result[0].theta.A.evaluate()).to.be.equal(Math.log(25));
+    });
+
+    it('should return correct result for matching output', () => {
+      let result = engine.query(ProgramFactory.literal('log(25, ' + Math.log(25) + ')'));
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('theta');
+      expect(result[0].theta).to.be.empty;
+    });
+
+    it('should return empty array for non-match', () => {
+      let result = engine.query(ProgramFactory.literal('log(25, 2)'));
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(0);
+    });
+  }); // log/2
+
   describe('succ/2', () => {
     it('should return correct result for variable replacement 1', () => {
       let result = engine.query(ProgramFactory.literal('succ(5, A)'));
