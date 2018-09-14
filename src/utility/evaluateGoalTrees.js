04825e6e643fbcd0cb07821aef1aa51bd678feb8
Sam Yong <samyonggit@gmail.com>
2018-09-14 21:31:28 +0100

    fix missing reference in evaluateGoalTree

--- src/utility/evaluateGoalTrees.js
+++ src/utility/evaluateGoalTrees.js
@@ -3,6 +3,7 @@
   the BSD 3-Clause license. For more info, please see https://github.com/mauris/lps.js
  */
 
+const lpsRequire = require('../lpsRequire');
 const ConjunctionMap = lpsRequire('engine/ConjunctionMap');
 
 /**
